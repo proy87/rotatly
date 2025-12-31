c865cc5cce133b2b015ed950ccd8980d6831a656
Pavlo Roik <paulroyik@gmail.com>
2025-12-31 18:19:56 +0200

    Improved settings

--- rotatly/settings.py
+++ rotatly/settings.py
@@ -130,3 +130,39 @@ STORAGES = {
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+STATIC_ROOT = BASE_DIR / 'static_production'
+
+DEBUG = os.getenv("DEBUG", 'True').lower() == 'true'
+
+if DEBUG:
+    ALLOWED_HOSTS = []
+
+    # Database
+    # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+    # Database
+    # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+            'OPTIONS': {
+                'sslmode': 'require',
+            },
+        }
+    }
