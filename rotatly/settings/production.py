83d6574d46afdb4115b2194cd50cd707312abe5a
Pavlo Roik <paulroyik@gmail.com>
2025-12-31 14:29:46 +0200

    Change database connection params

--- rotatly/settings/production.py
+++ rotatly/settings/production.py
@@ -15,9 +15,6 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT", "5432"),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
     }
 }
 
