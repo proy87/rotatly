462549a9d7d6fc36fa3836f3220d8ecb3a5f8ffb
Pavlo Roik <paulroyik@gmail.com>
2025-12-31 18:22:41 +0200

    Improved settings

--- rotatly/wsgi.py
+++ rotatly/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rotatly.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rotatly.settings')
 
 application = get_wsgi_application()
