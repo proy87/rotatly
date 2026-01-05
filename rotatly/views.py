42429afd3feb0ab71a3cf759e6a6a9a4e9a5fac6
Pavlo Roik <paulroyik@gmail.com>
2026-01-05 09:45:55 +0200

    Added constraint on number of moves

--- rotatly/views.py
+++ rotatly/views.py
@@ -66,7 +66,7 @@ def rotatly(request, date=None):
                        is_solved=is_solved(game.board, outline.board, pre_moves, game.disabled_nodes),
                        nodes=[[(e, game.disabled_nodes.get(str(e), dict())) for e in range(i, i + size - 1)] for i in
                               range(1, (size - 1) ** 2, size - 1)],
-                       moves_max_num=game.moves_min_num * 10,
+                       moves_max_num=game.moves_min_num * 2,
                        cw_symbol=CW_SYMBOLS[0],
                        ccw_symbol=CCW_SYMBOLS[0],
                        archived=date is not None,
