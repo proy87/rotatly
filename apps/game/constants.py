c0ec3089e3c9f3b5d040efc7266f68625b3a913c
Pavlo Roik <paulroyik@gmail.com>
2026-06-23 16:55:45 +0300

    Changed the starting date.

--- apps/game/constants.py
+++ apps/game/constants.py
@@ -3,6 +3,6 @@ import datetime
 CUSTOM_GAME_STR = 'abcdefghkmnpqrstuvwxyz123456789'
 CUSTOM_GAME_SLUG_LENGTH = 7
 
-START_DATE = datetime.datetime(2026, 6, 23)
+START_DATE = datetime.datetime(2026, 6, 22)
 DATE_FORMAT = '%B %d, %Y'
 JS_DATE_FORMAT = '%Y-%m-%d'
